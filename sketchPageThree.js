a648a6e4954992854de0b0e47b1d81590e620554
Xiqian <xiqianli.art@gmail.com>
2021-10-03 18:54:30 -0700

    test4

--- sketchPageThree.js
+++ sketchPageThree.js
@@ -7,9 +7,15 @@ var imgX, imgY
 var active = false;
 var switchStatus = false;
 var nextButton,cancelButton;
-var imgFrameX,imgFrameY,imgFrameX,imgFrameY;
+var imgFrameOneX,imgFrameOneY,imgFrameOneX,imgFrameOneY;
+var frameOne;
+var col;
+var offset,ratio;
+var constraints;
+
 
 // var img;
+
 
 
 var imgFrameOne,imgFrameTwo,imgFrameThree;
@@ -20,16 +26,13 @@ function preload() {
   imgHome = loadImage('img/home.svg');
   imgPic = loadImage('img/image.svg');
 
-  imgPicOne = loadImage('img/picOne.jpeg');
-  imgPicTwo = loadImage('img/picTwo.jpeg');
-  imgPicThree = loadImage('img/picThree.jpg');
+  imgPicOne = loadImage('img/picOne.png');
+  imgPicTwo = loadImage('img/picTwo.png');
+  imgPicThree = loadImage('img/picThree.png');
 
   imgArrowLeft = loadImage('img/arrow-left-circle.svg');
   imgArrowRight = loadImage('img/arrow-right-circle.svg');
 
-  // imgFrameOne = loadImage('img/picFrameOne.png');
-  // imgFrameTwo = loadImage('img/picFrameTwo.png');
-  // imgFrameThree = loadImage('img/picFrameThree.png');
 
 }
 
@@ -49,62 +52,118 @@ function setup() {
   //capture.hide();
 
   imgX = windowWidth * 1/2 - 40;
-  imgY = windowHeight-400;
+  imgY = windowHeight-450;
 
   imgW = 120;
   imgH = 180;
 
   // img = createImg('img/test.png','testImag');
-
   imgFrameOne = createImg('img/picFrameOne.png','imgFrameOne');
   imgFrameTwo = createImg('img/picFrameTwo.png','imgFrameTwo');
   imgFrameThree = createImg('img/picFrameThree.png','imgFrameThree');
 
-
-
-  imgFrameX =     160;
-  imgFrameY =     windowHeight-400;
-
-  imgFrameW = 120;
-  imgFrameH = 160;
+  artButton = createImg('img/artButton.png','artButton');
+  frameButton = createImg('img/frameButton.png','frameButton');
+  moreButton = createImg('img/moreButton.png','moreButton');
 
 
 
+  frameOne = {
+    locX:'160',
+    locY: windowHeight-450,
+    w:'120',
+    h:'180'
+  }
 
-  
+  frameTwo = {
+    locX:windowWidth * 1/2 - 40,
+    locY: windowHeight-450,
+    w:'120',
+    h:'180'
+  }
+
+  frameThree = {
+    locX: windowWidth -120 -160,
+    locY: windowHeight-450,
+    w:'120',
+    h:'180'
+  }
+
+// pic
+  picOne = {
+    locX:'160',
+    locY: windowHeight-450,
+    w:'120',
+    h:'180'
+  }
+
+  picTwo = {
+    locX:windowWidth * 1/2 - 40,
+    locY: windowHeight-450,
+    w:'120',
+    h:'180'
+  }
+
+  picThree = {
+    locX: windowWidth -120 -160,
+    locY: windowHeight-450,
+    w:'120',
+    h:'180'
+  }
+
+  col = color(255,255,255);
+  offset = -30;
+  ratio = 1.7;
 }
 
 function draw() {
   background(255);
   image(capture, 0, 0, windowWidth, windowHeight);
 
-  nextButton = createButton('NEXT');
-  nextButton.size(200,100);
-  nextButton.style('font-size','36px','stroke','red','text-color','grey');
-  nextButton.position(windowWidth - nextButton.width - 100, windowHeight * 1/10);
+  saveButton = createButton('SAVE');
+  saveButton.size(200,100);
+  saveButton.style('font-size','36px','background-color',col);
+  saveButton.position(windowWidth - saveButton.width - 100, windowHeight * 1/30);
 
   cancelButton = createButton('CANCEL');
   cancelButton.size(200,100);
-  cancelButton.style('font-size','36px','stroke','red','text-color','grey');
-  cancelButton.position(100, windowHeight * 1/10);
-
-  // img.position(100,100);
+  cancelButton.style('font-size','36px','background-color',col);
+  cancelButton.position(100, windowHeight * 1/30);
 
 
+  // top nav
+  fill(255);
+  noStroke();
+  rect(0,0,windowWidth,200);
 
+  // buttom nav
 
-  // filter(INVERT);
+  fill(255);
+  noStroke();
+  rect(0,windowHeight-200,windowWidth,200);
 
   // pic nav bar
   fill(255, 255, 255,80);
   noStroke();
-  rect(0,windowHeight-450,windowWidth,400);
+  rect(0,windowHeight-500,windowWidth,400);
 
   // arrow-left-circle
 
   image(imgPicTwo,imgX,imgY,imgW,imgH);
-  image(imgArrowLeft,50,windowHeight-380,100,100);
-  image(imgArrowRight,windowWidth-50-80,windowHeight-380,100,100);
+
+  image(imgArrowLeft,50,windowHeight-390,100,100);
+  image(imgArrowRight,windowWidth-50-80,windowHeight-390,100,100);
+
+
+  // three buttons
+  artButton.size(300,150);
+  artButton.position(50,windowHeight-180);
+
+  frameButton.size(300,150);
+  frameButton.position(windowWidth * 1/2 - 150,windowHeight-180);
+
+  moreButton.size(300,150);
+  moreButton.position(windowWidth - 300 - 50,windowHeight-180);
 
   if(switchStatus) {
 
@@ -113,35 +172,51 @@ function draw() {
     imgFrameThree.show();
 
 
-    imgFrameOne.position(imgFrameX,imgFrameY);
-
-    imgFrameOne.size(imgFrameW,imgFrameH);
-    imgFrameTwo.position(windowWidth * 1/2 - 40,windowHeight-400);
-    imgFrameTwo.size(120,160);
-    imgFrameThree.position(windowWidth -120 -160,windowHeight-400)
-    imgFrameThree.size(120,160);
+    imgFrameOne.position(frameOne.locX,frameOne.locY);
+    imgFrameOne.size(frameOne.w,frameOne.h);
 
 
-    imgFrameOne.mousePressed(frameNow);
-    // imgFrameTwo.mousePressed(frameNow);
-    // imgFrameThree.mousePressed(frameNow);
+    imgFrameTwo.position(frameTwo.locX,frameTwo.locY);
+    imgFrameTwo.size(frameTwo.w,frameTwo.h);
+
+    imgFrameThree.position(frameThree.locX,frameThree.locY);
+    imgFrameThree.size(frameThree.w,frameThree.h);
+
+    imgFrameOne.mousePressed(frameOneNow);
+    imgFrameTwo.mousePressed(frameTwoNow);
+    imgFrameThree.mousePressed(frameThreeNow);
 
 
   } else {
-    image(imgPicOne,160,windowHeight-400,120,160);
-    image(imgPicThree,windowWidth -120 -160,windowHeight-400,120,160);
+    image(imgPicOne,picOne.locX,picOne.locY,picOne.w,picOne.h);
+    image(imgPicThree,picThree.locX,picThree.locY,picThree.w,picThree.h);
 
     imgFrameOne.hide();
     imgFrameTwo.hide();
     imgFrameThree.hide();
 
   }
+
+
   // click next
-  nextButton.mousePressed(switchFrame);
+  saveButton.mousePressed(takesnap);
+
+  frameButton.mousePressed(switchFrame)
+
+
+
+
+
+
 
 
 }
 
+function takesnap() {
+  image(constraints,0,0);
+}
+
+// drag img
 function mouseClicked() {
   if (mouseX>imgX && mouseX<imgX+120 && mouseY>imgY && mouseY<imgY+160) {
     active = true;
@@ -168,18 +243,67 @@ function mouseReleased() {
 
 function switchFrame() {
   switchStatus = true;
+  console.log("switch success")
 }
 
-function frameNow() {
-  imgFrameX = imgX;
-  imgFrameY = imgY;
+function frameOneNow() {
 
-  imgFrameW = 240*1.6;
-  imgFrameH = 360*1.6;
 
-  console.log('yes');
-  console.log(imgX)
+  frameOne.locX = imgX+offset;
+  frameOne.locY = imgY+offset;
+  frameOne.w= 240*ratio;
+  frameOne.h = 360*ratio;
+
+  frameTwo.locX = windowWidth * 1/2 - 40;
+  frameTwo.locY = windowHeight-450;
+  frameTwo.w = 120;
+  frameTwo.h = 180;
+
+  frameThree.locX = windowWidth -120 -160,
+  frameThree.locY = windowHeight-450;
+  frameThree.w = 120;
+  frameThree.h = 180;
 }
+
+
+function frameTwoNow() {
+  frameTwo.locX = imgX+offset;
+  frameTwo.locY = imgY+offset;
+  frameTwo.w= 240*ratio;
+  frameTwo.h = 360*ratio;
+
+  frameOne.locX = 160;
+  frameOne.locY = windowHeight-450;
+  frameOne.w = 120;
+  frameOne.h = 180;
+
+
+  frameThree.locX = windowWidth -120 -160;
+  frameThree.locY = windowHeight-450;
+  frameThree.w = 120;
+  frameThree.h = 180;
+}
+
+function frameThreeNow() {
+  frameThree.locX = imgX+offset;
+  frameThree.locY = imgY+offset;
+
+  frameThree.w= 240*ratio;
+  frameThree.h = 360*ratio;
+
+  frameOne.locX = 160;
+  frameOne.locY = windowHeight-450;
+  frameOne.w = 120;
+  frameOne.h = 180;
+
+  frameTwo.locX = windowWidth * 1/2 - 40;
+  frameTwo.locY = windowHeight-450;
+  frameTwo.w = 120;
+  frameTwo.h = 180;
+}
+
+
+
 
 
 
